dd6a3686287d9a4ddccb02a767da30e81b746f95
Shuvo_Td <shuvotalukder7466@gmail.com>
2021-07-29 08:51:07 +0600

    second commit

---
+++ Node-tutorial/app.js
@@ -0,0 +1,17 @@
+// GLOBALS - NO WINDOW !!!
+
+//const { createReadStream } = require("fs");
+//const { result } = require("lodash");
+
+// __dirname - path to current dictionary
+//__filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process -info about env where the program is being executed
+
+// __dirname
+/*console.log(module);
+setInterval(() => {
+  console.log("Hi Everyone");
+}, 1500);*/
+// const { clear } = require("console");
